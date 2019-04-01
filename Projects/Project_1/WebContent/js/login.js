7db2f810c73d10229951c7dd84bb3574886ac612
Mathew Schweigardt <schweigardt96@gmail.com>
2019-04-01 09:58:16 -0500

    Big commit

--- Projects/Project_1/WebContent/js/login.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("Form").onsubmit = () => {
-    window.alert(
-        "Username: " + document.getElementById("Username").value
-        + "\nPassword: " + document.getElementById("Password").value
-        + "\nRemeber me?: " + document.getElementById("Remeber").checked
-    );
-}
